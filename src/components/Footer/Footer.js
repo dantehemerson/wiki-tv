f72fbdd9dac9205545334efb217f57a58efe92ef
Dante Calderón <dantehemerson@gmail.com>
2018-04-28 03:44:15 -0500

    Componente Footer añadido

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './Footer.css';
+
+function Footer(props) {
+	return(
+		<footer className="footer">
+			<div className="container">
+				<p>&copy; 2018 - <a className="has-text-primary has-text-weight-semibold" href="https://twitter.com/dantehemerson">Dante Calderón</a></p>
+			</div>
+		</footer>
+		);
+}
+
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Estilos globales para toda la aplicacion
 import './index.css';
-import App from './App';
+
+import App from './components/App/App';
+
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
