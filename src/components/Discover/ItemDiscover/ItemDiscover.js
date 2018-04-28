9bc2e5c1cd7e91883dc422bd311464d5692a83d5
Dante Calderón <dantehemerson@gmail.com>
2018-04-28 16:40:26 -0500

    Conectando con la API

--- src/components/Discover/ItemDiscover/ItemDiscover.js
+++ src/components/Discover/ItemDiscover/ItemDiscover.js
@@ -3,17 +3,19 @@ import React, { Component } from 'react';
 class ItemDiscover extends Component {
 
 	render() {
+		let data = this.props.data;
 		return(
+
 			<div className="column is-half-mobile is-4-tablet is-3-desktop">
 			<div className="card">
 				<div className="card-image">
 					<figure className="image is-2by3">
-						<img src="https://image.tmdb.org/t/p/w300/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg" alt="Imagen de la pelicula"/>
+						<img src={data.poster_path === null ? 'http://via.placeholder.com/300x450' : `https://image.tmdb.org/t/p/w300/${data.poster_path}`} alt="Imagen de la pelicula"/>
 					</figure>
 				</div>
 				<div className="card-content">
 					<div className="media-content">
-				    	<p className="title is-4">The BigBang Theory</p>
+				    	<p className="title is-4">{ data.title }</p>
 				    </div>
 			      	<time dateTime="2016-1-1">1 Jan 2016</time>
 				    <div className="content">
--- src/components/Discover/Movies/Movies.js
+++ src/components/Discover/Movies/Movies.js
@@ -5,24 +5,52 @@ import ItemDiscover from '../ItemDiscover/ItemDiscover';
 import './Movies.css';
 
 class Movies extends Component {
+	constructor(...props) {
+		super(...props);
+
+		this.state = {
+			movies: []
+		}
+	}
+
+	componentDidMount() {
+		const key = '954c577b7ec500b5c660c0212e03029f';
+
+		// Get a date range between today and one month ago to dynamically update the link for the request
+		let todayDate = new Date();
+		let today = todayDate.getFullYear() + '-' + (todayDate.getMonth() + 1) + '-' + todayDate.getDate();
+		let oneMonthAgo = (todayDate.getMonth() === 0 ? todayDate.getFullYear() - 1 : todayDate.getFullYear()) + '-' + (todayDate.getMonth() === 0 ? todayDate.getMonth() + 12 : todayDate.getMonth()) + '-' + todayDate.getDate();
+
+		fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=${oneMonthAgo}&primary_release_date.lte=${today}`)
+		  .then(response => {
+		    if (response.status !== 200) {
+		      console.log('Error: ' + response.status);
+		      return;
+		    }
+
+		    response.json().then(data => {
+		      const movies = data.results;
+		      this.setState({ movies });		      
+		    });
+
+		  })
+		  .catch(err => {
+		    console.log('Fetch Error :-S', err);
+		  })
+	}
 
 	render() {
 		return(
-			<div>
-				
-
+			<div>				
 				<div className="container">
 					<div className="columns is-multiline is-centered is-mobile column-items">
-					<ItemDiscover/>
-					<ItemDiscover/>
-					<ItemDiscover/>
-					<ItemDiscover/>
-					<ItemDiscover/>
-					<ItemDiscover/>
-					<ItemDiscover/>
-					<ItemDiscover/>
-					<ItemDiscover/>
-					<ItemDiscover/>
+						{
+							this.state.movies.map((movie, index) => {
+								return(
+									<ItemDiscover data={ movie } key={ index } />
+								);
+							})
+						}
 					</div>
 				</div>
 			</div>
