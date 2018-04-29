c4518fd57a4e14f9546a616e36442d17a3c4d27f
Dante Calderón <dantehemerson@gmail.com>
2018-04-29 16:03:51 -0500

    Padding in poster

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -59,16 +59,16 @@ class Movie extends Component {
 				<div className="backdrop" style={ this.state.movie.backdrop_path ? {backgroundImage: `url(https://image.tmdb.org/t/p/w1280/${this.state.movie.backdrop_path}`} : {backgroundImage: `https://www.laprensagrafica.com/__export/1519481573255/sites/prensagrafica/img/2018/02/24/rachel_crop1519481522365.jpg_525981578.jpg`} }>
 					<div className="backdrop-backcolor">
 						<div className="container movie-container">					
-							<div className="columns is-centered is-multiline">
+							<div className="columns is-centered is-multiline is-mobile">
 								<div className="column is-12 movie-data-header is-hidden-tablet">
 									<h1 className="title is-3 has-text-centered">{ this.state.movie.title }</h1>
 								</div>
-								<div className="column is-4">
+								<div className="column is-4-tablet is-8-mobile">
 									<figure className="image is-2by3">
-										<img src={this.state.movie.poster_path ? `https://image.tmdb.org/t/p/w780/${this.state.movie.poster_path}` : 'http://via.placeholder.com/300x450'} alt="Imagen de la pelicula"/>
+										<img className="poster-movie" src={this.state.movie.poster_path ? `https://image.tmdb.org/t/p/w780/${this.state.movie.poster_path}` : 'http://via.placeholder.com/300x450'} alt="Imagen de la pelicula"/>
 									</figure>
 								</div>
-								<div className="column movie-data-header">
+								<div className="column movie-data-header is-10-mobile">
 									<h1 className="title is-2 is-hidden-mobile">{ this.state.movie.title }</h1>	
 
 									<div className="place">
