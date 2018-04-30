1245cf624f35b69355b3a343d4a7739111015269
Dante Calderón <dantehemerson@gmail.com>
2018-04-29 19:48:44 -0500

    Añadi los nombres de los titlos de informacion

--- src/components/Movie/Information/Information.js
+++ src/components/Movie/Information/Information.js
@@ -7,7 +7,38 @@ class Information extends Component {
 	render() {
 		return(
 			<div className="container">
-				Information
+				<ul>
+					<li>
+						<p><strong>Titulo original</strong></p>
+						<p>Avengers infinity war</p>
+					</li>
+					<li>
+						<p><strong>Estado</strong></p>
+						<p>Avengers infinity war</p>
+					</li>
+					<li>
+						<p><strong>Lenguaje Original</strong></p>
+						<p>Avengers infinity war</p>
+					</li>
+					<li>
+						<p><strong>Duración</strong></p>
+						<p>Avengers infinity war</p>
+					</li>
+					<li>
+						<p><strong>Presupuesto</strong></p>
+						<p>Avengers infinity war</p>
+					</li>
+					<li>
+						<p><strong>Ingresos</strong></p>
+						<p>Avengers infinity war</p>
+					</li>
+					<li>
+						<p><strong>Sitio Oficial</strong></p>
+						<p>Avengers infinity war</p>
+					</li>
+
+
+				</ul>
 			</div>
 		);
 	}
